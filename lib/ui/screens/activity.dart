2579801c40bd11a6776faf1c46e135babea45550
Hamza Azmat <86201459+hamzaazmat01@users.noreply.github.com>
2021-12-16 12:27:25 +0500

    @12:27PM

--- lib/ui/screens/activity.dart
+++ lib/ui/screens/activity.dart
@@ -11,78 +11,266 @@ class _ActivityState extends State<Activity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Manualidad'),
-      ),
-      body: new Column(
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(35),
-                    topRight: Radius.circular(35))),
-            child: Image.asset(
-              'assets/images/flower.jpg',
-              fit: BoxFit.cover,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(color: Color(0xFFF0ECFC)),
+              height: 480, //380,
+              width: MediaQuery.of(context).size.width,
             ),
-            // Column(
-            //   children: [
-            //     Container(
-            //       child: ListTile(
-            //         leading: new CircleAvatar(
-            //           child: Image.asset('assets/images/flower.jpg'),
-            //         ),
-            //         title: Text(title),
-            //         subtitle: Text(subtile),
-            //         trailing: Icon(Icons.arrow_forward),
-            //       ),
-            //     ),
-            //   ],
-            // )
-          ),
-          Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35),
-                      topRight: Radius.circular(35))),
-              child: Column(
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Materials',
-                        style: TextStyle(fontSize: 20),
+                  TextButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.arrow_back_ios_outlined,
+                        color: Colors.black87,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                    child: Text(
+                      "Manualidad",
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(140, 0, 0, 0),
+                    child: CircleAvatar(
+                      radius: 22,
+                      backgroundColor: Colors.orange,
+                      child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: AssetImage('assets/images/12.jpg'),
                       ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(14, 150, 0, 0),
+              child: GestureDetector(
+                onTap: () {
+                  print("Youtube Link for Video");
+                },
+                child: Container(
+                  height: 230,
+                  width: 380,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(40)),
+                  child: Card(
+                    child:
+                        Image.asset('assets/images/ss.jpg', fit: BoxFit.cover),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            ///////////////////////////////////card2//////////////////////
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 390, 0, 0),
+                child: Container(
+                  height: 230,
+                  width: 360,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 32, 0, 16),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Materials",
+                                style: TextStyle(
+                                    letterSpacing: 1,
+                                    color: Colors.white,
+                                    fontSize: 22),
+                              ),
+                            )),
+                      ),
+
+                      ///////////////////////// 1X ////////////
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 6, 0, 0),
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("1x"),
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.circular(7)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
+                              child: Text("Tejirasas"),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      ///////////////////////////2x/////////////
+
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 6, 0, 0),
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("1x"),
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.circular(7)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
+                              child: Text("Tejirasas"),
+                            )
+                          ],
+                        ),
+                      ),
+
+                      /////////////////////////////3x///////////////////////////////
+
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 6, 0, 0),
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("1x"),
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.circular(7)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
+                              child: Text("Tejirasas"),
+                            )
+                          ],
+                        ),
+                      ),
+                      ////////////////////////////////3x////////////
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 6, 0, 0),
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("1x"),
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade200,
+                                  borderRadius: BorderRadius.circular(7)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
+                              child: Text("Tejirasas"),
+                            )
+                          ],
+                        ),
+                      ),
+                      //////////////////////////
                     ],
                   ),
-                  Text(
-                      '1x Tijeras \n 2x Rollos de papel \n 1x Pintura Acrílica \n 1x Pincel \n 1x Rollo de cartón A4 '),
-                ],
-              )),
-          Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(35),
-                    topRight: Radius.circular(35))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Instructions',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
                 ),
-                Text('Seguiremos con los relieves. Parala caja. En los hu')
-              ],
+              ),
             ),
-          ),
-        ],
+
+            ///////////////////////////////card 3///////////////////////////////
+
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 640, 0, 20),
+                child: Container(
+                  height: 230,
+                  width: 360,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(38, 32, 0, 16),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Instructions",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 22),
+                              ),
+                            )),
+                      ),
+
+                      ///////////////////////// 1X ////////////
+
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 4, 16, 0),
+                        child: Text(
+                          'Lorem Ipsum ifinibus mi. Phasellus vestibulum justo non risus faucibus dictum. Proin quis commodo urna, vitae interdum justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis velit justo, condimentum nec quam a, fringilla sollicitudin ex.Aliquam orci purus',
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+
+                      //////////////////////////
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/screens/detailsScreen.dart
+++ lib/ui/screens/detailsScreen.dart
@@ -6,65 +6,153 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: Text(
-          'Details',
-          // style: Theme.of(context).appBarTheme.textTheme.bodyText1,
-        ),
-        // actions: [
-        //   IconButton(
-        //       icon: Icon(Icons.forward),
-        //       onPressed: () {
-        //         Navigator.push(
-        //           context,
-        //           MaterialPageRoute(builder: (context) => FillTheFormFound()),
-        //         );
-        //       }),
-        // ],
-      ),
-      body: SingleChildScrollView(
-        child: new Stack(
-          children: [
-            Container(
-                height: 380,
-                width: MediaQuery.of(context).size.width,
-                child: FittedBox(
-                    fit: BoxFit.cover,
-                    child: Image.asset('assets/images/flower.jpg'))),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+      body: new Stack(
+        children: [
+          Container(
+              height: 300, //380,
+              width: MediaQuery.of(context).size.width,
+              child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: Image.asset('assets/images/12.jpg'))),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.arrow_back_ios_outlined,
+                      color: Colors.white,
+                    )),
+                Column(
+                  //mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+                      child: Text(
+                        "Canto",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 26),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.blueAccent.shade700,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(1.2, 1.5, 1.2, 1.5),
+                        child: Text(
+                          "Maramarasdsd",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(140, 0, 0, 0),
+                  child: CircleAvatar(
+                    radius: 22,
+                    backgroundColor: Colors.yellow,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('assets/images/flower.jpg'),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+            child: Center(
               child: Container(
                 height: MediaQuery.of(context).size.height / 1.5,
-                width: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width / 1.13,
                 child: Card(
                     shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(35),
-                            topRight: Radius.circular(35))),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
                     child: Column(
                       children: [
                         Container(
-                          child: Row(
-                            children: [
-                              new CircleAvatar(),
-                              Column(
-                                children: [Text('Title'), Text('Subtitle')],
-                              )
-                            ],
+                          child: Padding(
+                            padding: const EdgeInsets.all(29.0),
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                    onPressed: () {},
+                                    child: Icon(
+                                      Icons.thumb_up_rounded,
+                                      color: Colors.white,
+                                      size: 18,
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                    )),
+                                Center(
+                                  child: Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(38, 0, 0, 0),
+                                    child: Column(
+                                      // mainAxisAlignment: MainAxisAlignment.start,
+                                      // crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Lectura',
+                                          style: TextStyle(
+                                              fontSize: 22,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text('United America')
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                        Text(
-                            'asdaaaaaaaaaaaaaa sdkjfie eefjeifj fjeifjeif fjeifjiejf fjeifjeif fje98jf9ejf'),
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Divider(
+                            height: 0.1,
+                            thickness: 1,
+                            color: Colors.black,
+                          ),
+                        ),
+                        new Container(
+                            child: Expanded(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(16, 6, 16, 0),
+                              child: Text(
+                                "Lorem Ipsum ifinibus mi. Phasellus vestibulum justo non risus faucibus dictum. Proin quis commodo urna, vitae interdum justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis velit justo, condimentum nec quam a, fringilla sollicitudin ex.Aliquam orci purus, placerat sed dui vel, aliquam imperdiet lorem. Praesent quis massa orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla laoreet sed enim rhoncus pellentesque. Integer sollicitudin libero eget facilisis imperdiet. In quis imperdiet orci, malesuada eleifend erat. Mauris tempus sodales eros, consequat tincidunt quam commodo id. Donec faucibus mauris porttitor nisl posuere, in condimentum neque tempor. Ut at nunc condimentum, dignissim libero ut, tristique purus. Proin et porta sapien, id rhoncus neque. Nullam lacinia mollis elit vitae lacinia.Nullam bibendum iaculis congue. Praesent eget lorem porttitor, pellentesque leo vel, vulputate nulla. Donec velit enim, blandit eget lorem sed, suscipit egestas orci. Fusce vel volutpat metus, volutpat lobortis lacus. Etiam posuere lectus non dolor pulvinar rutrum. Vestibulum eu commodo ante. Duis ut tellus a tellus pellentesque varius. Mauris in sapien non turpis scelerisque sagittis ut eget massa. Nulla facilisis mi id libero condimentum congue. Cras id rutrum libero. Suspendisse vehicula placerat mi, vel vulputate tellus fringilla ac. Sed pellentesque nunc lorem, vel ornare nulla mattis id. Nam semper arcu ut massa interdum, vitae vestibulum ipsum eleifend. Nam nec volutpat magna, eget aliquet metus. Suspendisse blandit rhoncus enim, id commodo nunc ultrices quis. ",
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ),
+                        )),
                       ],
                     )),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
